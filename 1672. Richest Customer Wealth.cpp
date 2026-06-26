91496fc9ec3dc9df2e3c21f3ad8c016261ca54e9
Rudra Deepak <rudra763.org.in@gmail.com>
2026-06-26 11:04:43 +0530

    Create 1672. Richest Customer Wealth.cpp

---
+++ 1672. Richest Customer Wealth.cpp
@@ -0,0 +1,24 @@
+//1672. Richest Customer Wealth
+
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+
+        int maxWealth = 0;
+
+        for(int i = 0; i < accounts.size(); i++) {
+
+            int wealth = 0;
+
+            for(int j = 0; j < accounts[i].size(); j++) {
+                wealth += accounts[i][j];
+            }
+
+            if(wealth > maxWealth) {
+                maxWealth = wealth;
+            }
+        }
+
+        return maxWealth;
+    }
+};
